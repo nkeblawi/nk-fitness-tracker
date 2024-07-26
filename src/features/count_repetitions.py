427742653847710d9652902154b28442d6406f4d
nkeblawi <kebnab@gmail.com>
2024-07-26 11:19:17 -0400

    Added new count repetitions section in p4 notebook

--- src/features/count_repetitions.py
+++ src/features/count_repetitions.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from DataTransformation import LowPassFilter
+from src.features.DataTransformation import LowPassFilter
 from scipy.signal import argrelextrema
 from sklearn.metrics import mean_absolute_error
 
 pd.options.mode.chained_assignment = None
 
-
 # Plot settings
+import matplotlib.pyplot as plt
+
 plt.style.use("fivethirtyeight")
 plt.rcParams["figure.figsize"] = (20, 5)
 plt.rcParams["figure.dpi"] = 100
@@ -19,7 +19,7 @@ plt.rcParams["lines.linewidth"] = 2
 # Load data
 # --------------------------------------------------------------
 
-df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
+df = pd.read_pickle("data/interim/01_data_processed.pkl")
 df = df[df["label"] != "rest"]
 
 acc_r = df["acc_x"] ** 2 + df["acc_y"] ** 2 + df["acc_z"] ** 2
@@ -37,23 +37,6 @@ squat_df = df[df["label"] == "squat"]
 row_df = df[df["label"] == "row"]
 ohp_df = df[df["label"] == "ohp"]
 dead_df = df[df["label"] == "dead"]
-
-
-# --------------------------------------------------------------
-# Visualize data to identify patterns
-# --------------------------------------------------------------
-
-plot_df = bench_df
-
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_x"].plot()
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_y"].plot()
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_z"].plot()
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_r"].plot()
-
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_x"].plot()
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_y"].plot()
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_z"].plot()
-plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_r"].plot()
 
 
 # --------------------------------------------------------------
